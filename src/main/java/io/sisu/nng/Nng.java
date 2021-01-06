ab9eccf547f416e2d6b9a5182b192f5b9c55e9ed
Dave Voutila <voutilad@gmail.com>
2021-01-05 19:03:59 -0500

    Allow setting JNA properties via env vars
    Instead of fighting Gradle to get the proper JVM properties set for
    tests, just copy them from the environment if found.
    
    Maps:
    - JNA_DEBUG_LOAD --> jna.debug_load
    - JNA_LIBRARY_PATH --> jna.library.path

--- src/main/java/io/sisu/nng/Nng.java
+++ src/main/java/io/sisu/nng/Nng.java
@@ -3,6 +3,8 @@ package io.sisu.nng;
 import com.sun.jna.Native;
 import io.sisu.nng.internal.NngLibrary;
 
+import java.util.Properties;
+
 /**
  * Simple singleton access point. Only works in simple environments where libnng is installed in standard places.
  */
@@ -12,7 +14,17 @@ public class Nng {
     public static NngLibrary lib() {
         if (library == null) {
             System.err.println("Loading libnng...");
-            System.setProperty("jna.debug_load", "true");
+
+            final Properties props = System.getProperties();
+            if (!props.contains("jna.debug_load")) {
+                System.setProperty("jna.debug_load",
+                        System.getenv().getOrDefault("JNA_DEBUG_LOAD", ""));
+            }
+            if (!props.contains("jna.library.path")) {
+                System.setProperty("jna.library.path",
+                        System.getenv().getOrDefault("JNA_LIBRARY_PATH", ""));
+            }
+
             library = (NngLibrary) Native.load("nng", NngLibrary.class);
         }
         return library;
