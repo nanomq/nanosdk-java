ae3bdf8e5ce6f0af0efd90fe53f2b1ff94b6f37d
alvin1221 <alvin@emqx.io>
2022-08-28 12:15:32 +0800

    Add class TopicQos;

--- src/main/java/io/sisu/nng/mqtt/data/TopicQos.java
+++ src/main/java/io/sisu/nng/mqtt/data/TopicQos.java
@@ -1,13 +1,33 @@
 package io.sisu.nng.mqtt.data;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
 public class TopicQos {
     private String topic;
     private byte qos;
+
+
+    public String getTopic() {
+        return topic;
+    }
+
+    public void setTopic(String topic) {
+        this.topic = topic;
+    }
+
+    public byte getQos() {
+        return qos;
+    }
+
+    public void setQos(byte qos) {
+        this.qos = qos;
+    }
+
+    public TopicQos(String topic, byte qos) {
+        this.topic = topic;
+        this.qos = qos;
+    }
+
+    public TopicQos() {
+    }
 }
